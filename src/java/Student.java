c94545da830af20c9140efc1d6d2522f3f1e7cd9
Austin Hunt <austin.hunt.012@gmail.com>
2023-01-17 11:21:29 -0600

    finished part one junit testing

---
+++ src/java/Student.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+
+public class Student {
+
+    long id;
+    String name;
+    ArrayList<Integer> grades;
+
+
+    public Student(long id, String name){
+        this.id = id;
+        this.name = name;
+        this.grades = new ArrayList<>();
+
+    }
+
+
+    public long getId(){
+
+
+    }
+
+    public String getName(){
+
+    }
+
+    public void addGrade(int grade){
+
+    }
+
+    public ArrayList<Integer> getGrades(){
+
+    }
+
+    public double getGradeAverage(){
+
+    }
+
+
+
+
+
+}
